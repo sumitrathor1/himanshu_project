8aaf8548cb0b54bb303285898340bd791e28067f
Sumit Rathor <sumitrathor142272@gmail.com>
2025-04-05 13:05:22 +0530

    df

--- assets/pages/footer.php
+++ assets/pages/footer.php
@@ -4,7 +4,7 @@
 <footer class="footer">
     <div class="container">
         <div class="footer-section">
-            <h3>Exclusive</h3>
+            <h3 class="text-white">Exclusive</h3>
             <p>Subscribe</p>
             <p>Get 10% off your first order</p>
             <div class="subscribe">
@@ -16,13 +16,13 @@
             </div>
         </div>
         <div class="footer-section">
-            <h4>Support</h4>
+            <h4 class="text-white">Support</h4>
             <p>111 Bijoy sarani, Dhaka, Bangladesh</p>
             <p>exclusive@gmail.com</p>
             <p>+88015-88888-9999</p>
         </div>
-        <div class="footer-section">
-            <h4>Account</h4>
+        <div class=" footer-section">
+            <h4 class="text-white">Account</h4>
             <p>My Account</p>
             <p>Login / Register</p>
             <p>Cart</p>
@@ -30,14 +30,14 @@
             <p>Shop</p>
         </div>
         <div class="footer-section">
-            <h4>Quick Link</h4>
+            <h4 class="text-white">Quick Link</h4>
             <p>Privacy Policy</p>
             <p>Terms Of Use</p>
             <p>FAQ</p>
             <p>Contact</p>
         </div>
         <div class="footer-section">
-            <h4>Download App</h4>
+            <h4 class="text-white">Download App</h4>
             <p>Save $3 with App New User Only</p>
             <img src="./assets/images/qr.png" alt="">
             <img src="./assets/images/gps.webp" alt="" class="gps">
